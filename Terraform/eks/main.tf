edcba5d80063e809aaa408ab48b25989832fc8a9
Quy Tran <quytrandev@gmail.com>
2023-07-28 14:58:15 +0700

    Add permission for EBS CSI driver of EKS cluster, in order to create EC2 persistent volume.

--- Terraform/eks/main.tf
+++ Terraform/eks/main.tf
@@ -67,6 +67,7 @@ enable_irsa = true
     }
     aws-ebs-csi-driver = {
       most_recent = true
+      service_account_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${module.eks.cluster_name}-ebs-csi-controller"
     }
   }
 
@@ -100,3 +101,18 @@ enable_irsa = true
   tags = local.tags
 }
 
+module "ebs_csi_controller_role" {
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                       = "5.11.1"
+  create_role                   = true
+  role_name                     = "${module.eks.cluster_name}-ebs-csi-controller"
+  provider_url                  = replace(module.eks.cluster_oidc_issuer_url, "https://", "")
+  role_policy_arns              = [aws_iam_policy.ebs_csi_controller.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:${local.ebs_csi_service_account_namespace}:${local.ebs_csi_service_account_name}"]
+}
+
+resource "aws_iam_policy" "ebs_csi_controller" {
+  name_prefix = "ebs-csi-controller"
+  description = "EKS ebs-csi-controller policy for cluster ${module.eks.cluster_name}"
+  policy      = file("../ebs_csi_controller_iam_policy.json")
+}
