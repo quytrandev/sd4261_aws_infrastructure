dbdf8c815ac1f2a236b86c855d38551c8e4d58ea
HARVEYNASH\quytrant <quytrant@nashtechglobal.com>
2023-08-08 10:15:08 +0700

    Add ECR under provisioning

--- Terraform/eks/main.tf
+++ Terraform/eks/main.tf
@@ -90,6 +90,36 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 ###########################################################################################
+#ECR
+###########################################################################################
+module "ecr" {
+  source = "terraform-aws-modules/ecr/aws"
+
+  repository_name = "${module.tags_dev.name}-ecr"
+repository_image_tag_mutability = "MUTABLE" //allow to override image as pushing
+  repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+  repository_force_delete = true
+  
+}
+
+###########################################################################################
 #Elastic Kubernetes Services
 ###########################################################################################
 module "eks" {
