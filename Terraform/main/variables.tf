168192a1ac5ac828979f6a70149b5be5c303d293
Quy Tran <quytrant@nashtechglobal.com>
2023-10-24 14:53:33 +0700

    Update multi AZs for variables

--- Terraform/main/variables.tf
+++ Terraform/main/variables.tf
@@ -33,19 +33,19 @@ variable "cidr_block" {
 variable "public_subnet_cidrs" {
   type        = list(string)
   description = "Public Subnet CIDR values"
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   type        = list(string)
   description = "Private Subnet CIDR values"
-  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
 
 variable "intra_subnet_cidrs" {
   type        = list(string)
   description = "Intra Subnet CIDR values"
-  default     = ["10.0.5.0/24", "10.0.6.0/24"]
+  default     = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
 }
 
 variable "azs" {
