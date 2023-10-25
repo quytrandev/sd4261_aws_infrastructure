fdbc6fa827700e8a97b051c8148337489df87783
Quy Tran <quytrant@nashtechglobal.com>
2023-10-25 16:29:16 +0700

    Add new ECR

--- Terraform/main/main.tf
+++ Terraform/main/main.tf
@@ -47,56 +47,83 @@ module "vpc" {
 ###########################################################################################
 #EC2 Instance and supports
 ###########################################################################################
-module "ec2_instance" {
-  source = "terraform-aws-modules/ec2-instance/aws"
+# module "ec2_instance" {
+#   source = "terraform-aws-modules/ec2-instance/aws"
 
-  name                        = "${module.tags_dev.name}-ec2"
-  ami                         = "ami-00970f57473724c10" //Amazon Linux 2023 AMI
-  instance_type               = "t3.large"              //Large tier instance to meet Docker specs requirements
-  key_name                    = "jenkins-key"
-  monitoring                  = true
-  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
-  subnet_id                   = module.vpc.public_subnets[0] 
-  associate_public_ip_address = true
-}
-resource "aws_security_group" "ec2_sg" {
-  name   = "Security Group for EC2 under provisioning"
-  vpc_id = module.vpc.vpc_id
+#   name                        = "${module.tags_dev.name}-ec2"
+#   ami                         = "ami-00970f57473724c10" //Amazon Linux 2023 AMI
+#   instance_type               = "t3.large"              //Large tier instance to meet Docker specs requirements
+#   key_name                    = "jenkins-key"
+#   monitoring                  = true
+#   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+#   subnet_id                   = module.vpc.public_subnets[0] 
+#   associate_public_ip_address = true
+# }
+# resource "aws_security_group" "ec2_sg" {
+#   name   = "Security Group for EC2 under provisioning"
+#   vpc_id = module.vpc.vpc_id
 
- ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  ingress {
+#     from_port   = 8080
+#     to_port     = 8080
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = -1
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 ###########################################################################################
 #ECR
 ###########################################################################################
-module "ecr" {
+module "ecr_frontend" {
   source = "terraform-aws-modules/ecr/aws"
 
-  repository_name = "${module.tags_dev.name}-ecr"
-repository_image_tag_mutability = "MUTABLE" //allow to override image as pushing
+  repository_name = "${module.tags_dev.name}-ecr-frontend"
+repository_image_tag_mutability = "IMMUTABLE" //allow to override image as pushing
+  repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+  repository_force_delete = true
+  
+}
+
+module "ecr_backend" {
+  source = "terraform-aws-modules/ecr/aws"
+
+  repository_name = "${module.tags_dev.name}-ecr-backend"
+repository_image_tag_mutability = "IMMUTABLE" //allow to override image as pushing
   repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
   repository_lifecycle_policy = jsonencode({
     rules = [
